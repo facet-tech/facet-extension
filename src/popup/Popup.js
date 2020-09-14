e51328f7357aee31ad5ef002d8c30fd9b082ad52
mkotsollaris <mkotsollari@gmail.com>
2020-09-13 18:53:00 -0700

    save work

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,23 +1,13 @@
+/*global chrome*/
+
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { useAuth0 } from "@auth0/auth0-react";
 
 export default () => {
-    const {
-        user,
-        isAuthenticated,
-        loginWithRedirect,
-        logout,
-    } = useAuth0();
+    // chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+    //     // Use the token.
+    // });
 
     return <div>
-        <Button
-            variant="contained"
-            id="qsLoginBtn"
-            color="primary"
-            className="btn-margin"
-            onClick={() => loginWithRedirect()}
-        > Log in
-        </Button>
     </div>
 }
