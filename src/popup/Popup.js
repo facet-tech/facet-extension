dc374d8916b1a0d7dc55f3c67f96678ca32ab39c
mkotsollaris <mkotsollari@gmail.com>
2020-09-13 19:37:48 -0700

    save work

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -4,9 +4,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 export default () => {
-    // chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
-    //     // Use the token.
-    // });
+    chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+        // Use the token.
+    });
 
     return <div>
     </div>
