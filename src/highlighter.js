3927ebbcb5c640286fd5c157b5d33bb9a9532547
mkotsollaris <mkotsollari@gmail.com>
2020-11-19 15:00:24 -0800

    updated highlighter

--- src/highlighter.js
+++ src/highlighter.js
@@ -11,15 +11,16 @@ import isDevelopment from './utils/isDevelopment';
  * Performs transformation on client's DOM
  */
 const performDOMTransformation = () => {
-    if (isDevelopment()) {
-        return
-    }
     console.log('[facet.ninja][loader]');
 
     // push the body
     $('body').attr('style', function (i, s) {
         return (s || '') + `position: absolute !important;left: ${styles.drawerWidth}px !important;right: 0px !important;min-height: calc(100% - 96px) !important;overflow-x: initial !important;`;
     });
+
+    if (isDevelopment()) {
+        return
+    }
 
     $('*') && $('*').filter(function () {
         return $(this).css('position') === 'fixed' && this.id !== 'facetizer' &&
--- src/utils/isDevelopment.js
+++ src/utils/isDevelopment.js
@@ -1,4 +1,3 @@
 export default () => {
-    console.log('ISDEV', process)
     return process.env.NODE_ENV === "development";
 }
