3f658954072955ce25504bf7557dd225ba9fffbe
mkotsollaris <mkotsollari@gmail.com>
2020-11-26 12:27:01 -0800

    applied case-sensitive changes

---
+++ src/Authentication/ForgotPassword.js
@@ -0,0 +1,70 @@
+import { Auth } from "aws-amplify";
+import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import PopupContext from "../popup/PopupContext";
+import { authState as authStateConstant } from '../shared/constant';
+import fnLogoHorizontal from '../static/images/fn_horizontal_logo.png';
+import { Input, InputLabel, Button, Link } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import AppContext from "../AppContext";
+
+export default () => {
+    const { setCurrAuthState } = React.useContext(PopupContext);
+    const { authObject, setAuthObject } = React.useContext(AppContext);
+    const { register, errors, handleSubmit } = useForm({});
+    const [serverError, setServerError] = useState(undefined);
+
+    const onSubmit = async data => {
+        const { email } = data;
+
+        try {
+            const confirmSignUpResponse = await Auth.forgotPassword(email);
+            setCurrAuthState(authStateConstant.onPasswordReset);
+        } catch (error) {
+            setServerError(error.message);
+        }
+    };
+
+    return (
+        <React.Fragment>
+            <div style={{ textAlign: 'center' }}>
+                <img src={fnLogoHorizontal} />
+            </div>
+            <form onSubmit={e => e.preventDefault()}>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <Input
+                    style={{ width: "100%" }}
+                    id="email"
+                    name="email"
+                    aria-invalid={errors.email ? "true" : "false"}
+                    inputRef={register({
+                        required: "Please specify an email",
+                        pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: "Entered value does not match email format"
+                        }
+                    })}
+                    onChange={(e) => setAuthObject({
+                        ...authObject,
+                        email: e.target.value
+                    })}
+                    type="email"
+                />
+                {errors.email && <span role="alert">{errors.email.message}</span>}
+            </form>
+            <br />
+            {serverError && <Alert severity="error">{serverError}</Alert>}
+            <br />
+            <div>
+                <Button style={{ width: "100%" }} variant="contained" color="primary" type="submit" onClick={handleSubmit(onSubmit)}>
+                    Confirm email
+                </Button>
+            </div>
+            <div>
+                <br />
+            Don't have an account?
+            <Link href="#" onClick={() => setCurrAuthState(authStateConstant.signingUp)}>{' '}Sign up</Link>
+            </div>
+        </React.Fragment >
+    );
+}
---
+++ src/shared/Popup/triggerDOMReload.js
@@ -0,0 +1,7 @@
+/*global chrome*/
+
+export default () => {
+    chrome?.tabs?.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {}, async function (response) { });
+    });
+}
