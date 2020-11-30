d94da19253d3eee51fcd331055d99baffce8c2c1
mkotsollaris <mkotsollari@gmail.com>
2020-11-30 11:37:30 -0800

    updated FacetTreeSideBar.js

--- src/facetTreeSideBar/FacetTreeSideBar.js
+++ src/facetTreeSideBar/FacetTreeSideBar.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import $ from 'jquery';
 import TreeView from '@material-ui/lab/TreeView';
