e0d5a3f12f9ae505037b4370286f602d6623737c
mkotsollaris <mkotsollari@gmail.com>
2020-11-30 09:25:15 -0800

    save progress

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -1,8 +1,8 @@
-/*global chrome*/
+/* global chrome */
 
 import React, { useContext, useState, useEffect } from 'react';
-import PopupContext from './PopupContext';
 import styled from 'styled-components';
+import PopupContext from './PopupContext';
 import Main from './Main';
 import SignIn from '../authentication/SignIn';
 import Signup from '../authentication/Signup';
@@ -16,28 +16,30 @@ const StyledDiv = styled.div`
 `;
 
 export default () => {
-    const { currAuthState, jwt } = useContext(PopupContext);
+  const { currAuthState, jwt } = useContext(PopupContext);
 
-    let displayElement;
-    if (jwt || currAuthState === authStateConstant.signedIn) {
-        displayElement = <Main />;
-    } else if (currAuthState === authStateConstant.signingIn) {
-        displayElement = <SignIn />;
-    } else if (currAuthState === authStateConstant.signingUp) {
-        displayElement = <Signup />
-    } else if (currAuthState === authStateConstant.confirmingSignup) {
-        displayElement = <ConfirmationCode />
-    } else if (currAuthState === authStateConstant.signedIn) {
-        displayElement = <Main />
-    } else if (currAuthState === authStateConstant.onForgotPassword) {
-        displayElement = <ForgotPassword />;
-    } else if (currAuthState === authStateConstant.onPasswordReset) {
-        displayElement = <PasswordReset />;
-    } else {
-        displayElement = <SignIn />;
-    }
+  let displayElement;
+  if (jwt || currAuthState === authStateConstant.signedIn) {
+    displayElement = <Main />;
+  } else if (currAuthState === authStateConstant.signingIn) {
+    displayElement = <SignIn />;
+  } else if (currAuthState === authStateConstant.signingUp) {
+    displayElement = <Signup />;
+  } else if (currAuthState === authStateConstant.confirmingSignup) {
+    displayElement = <ConfirmationCode />;
+  } else if (currAuthState === authStateConstant.signedIn) {
+    displayElement = <Main />;
+  } else if (currAuthState === authStateConstant.onForgotPassword) {
+    displayElement = <ForgotPassword />;
+  } else if (currAuthState === authStateConstant.onPasswordReset) {
+    displayElement = <PasswordReset />;
+  } else {
+    displayElement = <SignIn />;
+  }
 
-    return <StyledDiv>
-        {displayElement}
+  return (
+    <StyledDiv>
+      {displayElement}
     </StyledDiv>
-}
+  );
+};
