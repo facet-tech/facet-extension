9b687c8e5eabe2a70e6eb4507fff1e06086f65cf
mkotsollaris <mkotsollari@gmail.com>
2021-01-27 14:21:35 -0800

    updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -18,7 +18,6 @@ import 'typeface-roboto';
 import FacetSnackbar from './shared/FacetSnackbar';
 import AmplifyService from './services/AmplifyService';
 import WelcomeAbroadStandalone from './shared/WelcomeAbroad/WelcomeAbroadStandalone';
-import TmpSimpleModal from './shared/TmpSimpleModal';
 
 Amplify.configure(awsExports);
 
@@ -54,8 +53,6 @@ chrome && chrome.runtime.onMessage && chrome.runtime.onMessage.addListener(
     }
   },
 );
-
-console.log('CHECKME', window.IN_PREVIEW, window.JSURL);
 
 // Adds facetizer into the client's DOM
 if (!document.getElementById('popup') && !document.getElementById('facet-welcome-page') && !window.IN_PREVIEW) {
@@ -160,36 +157,6 @@ if (document.getElementById(domIds.authentication) && isActivelyBeingDebugged(do
     document.getElementById(domIds.welcome),
   );
 } else if (document.getElementById(domIds.previewLoadingBar) && isActivelyBeingDebugged(domIds.previewLoadingBar)) {
-  // console.log('node', node);
-  // var previewNode = document.createElement('div');
-  // previewNode.innerHTML = "QQ";
-  // node.prepend(previewNode);
-
-  // useInterval(() => {
-  //   setProgress((oldProgress) => {
-  //     if (oldProgress === 100) {
-  //       return 100;
-  //     }
-  //     const diff = 35;
-  //     return Math.min(oldProgress + diff, 100);
-  //   });
-  // }, progress >= 100 ? null : 300);
-
-  // if (progress >= 100) {
-  //   onComplete();
-  // }
-
-  // var script = document.createElement('script');
-  // script.setAttribute('type', 'text/javascript');
-  // script.setAttribute('src', urrl);
-  // script.setAttribute('facet-extension-loaded', false);
-  // script.setAttribute('is-preview', true);
-
-  // node.appendChild(script);
-  ReactDOM.render(
-    <React.StrictMode>
-      <TmpSimpleModal />
-    </React.StrictMode>,
-    document.getElementById(domIds.previewLoadingBar),
-  );
+  var node = document.getElementsByTagName('html').item(0);
+  node.style.visibility = "visible";
 }
